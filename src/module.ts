d6436053a06a8603a34b9a9c7b30597c9765a7c1
sumihiro3 <sumihiro@gmail.com>
2023-11-11 15:07:11 +0900

    setup eslint prettier

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,22 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit';
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: 'nuxt-liff',
+    configKey: 'nuxtLiff',
   },
   // Default configuration options of the Nuxt module
-  defaults: {},
-  setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
+  defaults: {
+    liffId: '',
+    mock: false,
+  },
+  setup(options, nuxt) {
+    const resolver = createResolver(import.meta.url);
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-  }
-})
+    addPlugin(resolver.resolve('./runtime/plugin'));
+  },
+});

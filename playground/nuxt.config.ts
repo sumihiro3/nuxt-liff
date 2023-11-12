5aff97e7b48e6040d4e5fc7e7d449143d425ac0c
sumihiro3 <sumihiro@gmail.com>
2023-11-12 14:39:32 +0900

    コメント追加

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,9 +2,13 @@ import * as mockData from './mock-data';
 
 export default defineNuxtConfig({
   modules: ['../src/module'],
+  // モジュール設定
   liff: {
-    liffId: 'valid-liff-id-here',
+    // LIFF ID を設定してください
+    liffId: 'your-liff-id-here',
+    // モックモードを使う場合は true を設定してください
     mock: true,
+    // モックモードを使う場合、各 API のモックデータを設定できます
     mockData: {
       getProfile: mockData.getProfile(),
       getOS: 'android',
